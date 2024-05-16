3696b125ed6ffdf36aece313210b7e4625bb073e
Sun Jixiang <jixiang.sun@insa-lyon.fr>
2024-05-16 18:13:38 +0200

    Add HttpSession for student authentication

--- frontend/src/main/java/instructif/action/AuthentifierEleveAction.java
+++ frontend/src/main/java/instructif/action/AuthentifierEleveAction.java
@@ -1,6 +1,7 @@
 package instructif.action;
 
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
 
 import instructif.metier.modele.Eleve;
 import instructif.metier.service.Service;
@@ -12,10 +13,15 @@ public class AuthentifierEleveAction extends Action {
         // Authentification
         String mail = request.getParameter("mail");
         String password = request.getParameter("password");
-        
+
         Service service = new Service();
 
         Eleve e = service.connecterEleve(mail, password);
+
+        if (e != null) {
+            HttpSession session = request.getSession(true);
+            session.setAttribute("student", e);
+        }
 
         request.setAttribute("utilisateur", e);
     }
