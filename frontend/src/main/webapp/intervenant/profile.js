4282bc39ba8881bc3380def1271d95146b99c6f2
Sun Jixiang <jixiang.sun@insa-lyon.fr>
2024-05-24 16:10:46 +0200

    Fix navigation button typo and update profile display

--- frontend/src/main/webapp/intervenant/profile.js
+++ frontend/src/main/webapp/intervenant/profile.js
@@ -8,13 +8,20 @@ $(document).ready(() => {
     .done(function (response) {
       if (!response) return;
       const aRemplir = document.getElementById("content-div");
-      aRemplir.innerHTML = `<p>${response.nom} ${response.prenom}</p>
-            <p>Téléphone : ${response.telephone}</p>
-            <p>Type : ????</p>;
-            <p>Université : ????</p>
-            <p>Specialité : ????</p>
-            <p>Type d'établissement d'exercice : ????</p>
-            <p>Activité : ????</p>`;
+      aRemplir.innerHTML =
+        `<p>${response.nom} ${response.prenom}</p>` +
+        `<p>Téléphone : ${response.telephone}</p>` +
+        `<p>Type : ${response.type}</p>` +
+        (response.universite
+          ? `<p>Université : ${response.universite}</p>`
+          : "") +
+        (response.specialite
+          ? `<p>Specialité : ${response.specialite}</p>`
+          : "") +
+        (response.typeEtablissementExercice
+          ? `<p>Type d'établissement d'exercice : ${response.typeEtablissementExercice}</p>`
+          : "") +
+        (response.activite ? `<p>Activité : ${response.activite}</p>` : "");
     })
     .fail(function (error) {
       console.error("Erreur lors de la récupération des données :", error);
